142bc0147b57fd3e1d7ed8c8c90e44d3eb313d36
Stefano Ruth <stefano6262@gmail.com>
2017-03-20 13:38:28 +0100

    Add Cashier Db

--- app/User.php
+++ app/User.php
@@ -6,10 +6,11 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 
 class User extends Authenticatable
 {
-    use Notifiable, SoftDeletes;
+    use Notifiable, SoftDeletes, Billable;
 
     protected $guarded = [];
     protected $hidden = ['password', 'remember_token'];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,10 @@ class CreateUsersTable extends Migration
             $table->enum('gender', ['male', 'female', 'other']);
             $table->date('birthdate');
             $table->rememberToken();
+            $table->string('stripe_id')->nullable();
+            $table->string('card_brand')->nullable();
+            $table->string('card_last_four')->nullable();
+            $table->timestamp('trial_ends_at')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
